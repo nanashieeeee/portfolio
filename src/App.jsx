d70ddc87e093f4b051eed1b10724866eb83bbf28
Ejay Balina <ejbalina@gmail.com>
2025-03-01 04:46:00 +0800

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/header';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
-      <Router> {/* Wrap everything in Router */}
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
+      <Router>
+        {" "}
+        {/* Wrap everything in Router */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
     </>
   );
 }
